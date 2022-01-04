2f03df3827fa5973346f29fd5604f3cae727651e
Naegon <hamelgui@gmail.com>
2022-01-04 17:46:13 +0100

    Util method to read from json file

---
+++ src/main/java/com/company/Utils.java
@@ -0,0 +1,20 @@
+package com.company;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public abstract class Utils {
+    public static final String PATHFILE = "src/main/java/com/company/test.json";
+
+    public static Object readFromFile(String path) {
+        JSONParser parser = new JSONParser();
+
+        try {
+            return parser.parse(new FileReader(path));
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
